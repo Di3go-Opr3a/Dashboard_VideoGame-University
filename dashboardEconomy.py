153f3697a7450c285d4498a48dc8ab47c8f68dcc
Di3go-Opr3a <di3go.opr3a@gmail.com>
2023-04-27 23:17:31 +0200

    recap

--- dashboardEconomy.py
+++ dashboardEconomy.py
@@ -1,24 +1,42 @@
-import plotly.express as px
-import pandas as pd
-import plotly
+import pandas as pd # pip install pandas
 
-df = pd.read_excel('Book.xlsx')
+file = pd.read_csv("videogame.csv") # save date in file
 
-values = df['Result']
-names = df['Platform']
+# # Graph bar platform
+collumPlatform = file['Platform'] # list date collum 'Platform'
+wii, ps4, ds = 0, 0, 0
 
-fig = px.pie(df, 
-            values=values,
-            names = names,
-            title = "ciccio brllo")
+for x in collumPlatform:
+    if x == 'Wii':
+        wii += 1
+    elif x == 'PS4':
+        ps4 += 1
 
-fig.update_traces(
-    textposition = 'inside',
-    textinfo='percent+label'
-)
+print(wii, ps4)
+
+# import plotly.express as px
+# import pandas as pd
+# import plotly
+
+# df = pd.read_excel('Book.xlsx')
+# ages = df["Platform"]
+# print(ages)
+
+# values = df['Result']
+# names = df['Platform']
+
+# fig = px.pie(df, 
+#             values=values,
+#             names = names,
+#             title = "ciccio brllo")
+   
+# fig.update_traces(
+#     textposition = 'inside',
+#     textinfo='percent+label'
+# )
 
 # fig.update_layout(
 #     title_font_size = 10
 # )
 
-plotly.offline.plot(fig,filename='Piechart.html')
+# plotly.offline.plot(fig,filename='Piechart.html')
