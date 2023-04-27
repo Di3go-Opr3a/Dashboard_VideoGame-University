20d22e4daa2834ae6d0b1de83376b8094e129bbd
Di3go-Opr3a <di3go.opr3a@gmail.com>
2023-04-27 11:52:07 +0200

    Default

---
+++ dashboardEconomy.py
@@ -0,0 +1,18 @@
+import pandas as pd # installare pip pandas (lettura CSV)
+import streamlit as st # installare pip streamlit (visual 3D)
+
+# importazione dati da file csv in variabile tabella
+# 'CSV/' perchè il file sta in una cartella
+table = pd.read_csv('CSV/videogame.csv')
+
+# Creazione Pagina Dashboard
+st.set_page_config(page_title="Dashboard Economy | StrealmClix",
+                   page_icon=":bar_chart:",
+                   layout="wide") # logo che non abbiamo
+
+
+# per vedere la tabella nella schermata
+st.dataframe(table)
+
+
+# PER APRIRE SCHEDA WEB SCRIVERE COMANDO TERMINALE: streamlit run dashboardEconomy.py
