f9da16de33ae695c1f706d535b5e314554d1a9cd
Di3go-Opr3a <di3go.opr3a@gmail.com>
2023-04-30 12:34:02 +0200

    bla bla

--- dashboardEconomy.py
+++ dashboardEconomy.py
@@ -1,17 +1,19 @@
 import pandas as pd # pip install pandas
-import streamlit as st
-import matplotlib.pyplot as plt
+import streamlit as st # pip install streamlit
+import matplotlib.pyplot as plt # pip install matplotlib
 
 # def checkPlatform():
 #     global collumPlatform
 
-#     list = [] # save platform
+#     list = [] # salvataggio piattaforme
 #     for x in collumPlatform:
-#         if x not in list: # check if the variable isn't in list
-#             list.append(x) # save variable in list
+#         if x not in list: # controllo se la piattaforma non esiste
+#             list.append(x) # salvo la piattaforma in list
 
 #     print(list)
 
+# creo celle quante sono gli elementi riga 0, nella riga 1 di platform 
+# assegno 0
 def create0Value(platform):
     count = len(platform[0])
     platform[1] = [None] * count
@@ -21,43 +23,47 @@ def create0Value(platform):
     
     return platform
 
+# conto quanti sono i valori nel DataFrame per ogni elemento riga 0
 def countPlatform(platform):
     global collumPlatform
-
+    
     for row in range(len(platform[0])):
         for element in collumPlatform:
             if element == platform[0][row]:
                 platform[1][row] += 1
 
     return platform
-            
-            
-file = pd.read_csv("videogame.csv") # save date in file
 
-# GRAPH BAR WITH PLATFORM
-collumPlatform = file['Platform'] # list with date collum 'Platform'
+def piechart(platform):
+    # Distanza tra elementi
+    fig1, ax1 = plt.subplots()
+
+    explode = (0, 0, 0, 0.1, 0 , 0, 0, 0.1, 0, 0, 0)
+
+    # autopct = Numero percentuale mostrato
+    # startangle = parte da 90 gradi
+    ax1.pie(platform[1], labels=platform[0], autopct='%1.1f%%', startangle=90, explode=explode)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    
+    return st.pyplot(fig1)
+
+
+
+file = pd.read_csv("videogame.csv") # salvo dati in 'file'
+
+# DATAFRAME in vista
+st.dataframe(file)
+
+# GRAPH BAR CON 'PIATTAFORME'
+collumPlatform = file['Platform'] # lista dati con colona 'Platform'
 
 # checkPlatform()
-platform = [['PC', 'PS4', 'PS3', 'PS2', 'PSP', 'X360', 'XOne', 'DS', '3DS', 'Wii', 'WiiU'],
+platform = [['PC', 'PS4', 'PS3', 'PS2', 'PSP', 'X360', 'XOne', 'DS', 'WiiU', 'Wii', '3DS'],
             []]
 create0Value(platform)
 countPlatform(platform)
+# print(platform)
 
-print(platform  )
-
-
-# Pie chart, where the slices will be ordered and plotted counter-clockwise:
-labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-sizes = [15, 30, 45, 10]
-
-fig1, ax1 = plt.subplots()
-ax1.pie(platform[1], labels=platform[0], autopct='%1.1f%%',
-        shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-st.pyplot(fig1)
-
-
-
+piechart(platform)
 
 
